9c4c69233586e81af4e5ac71ec1705f575560816
sakshisahu07 <sakshisahu072@gmail.com>
2024-07-05 22:12:04 -0700

    useEffect useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,56 @@
-// =======================React Hook=========================================================
+// this is the exapmple of useeffect//
 
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+
+
+// const App=()=>{
+
+
+//   const[cnt,setCnt]=useState(0);
+
+
+// useEffect(()=>{
+//   setTimeout(()=>{
+//     setCnt(cnt+1);
+//   },5000)
+// },[])
+
+
+
+// return(
+//   <>
+//   <h1> this is there method of use effect {cnt}</h1>
+//   </>
+// )
+
+
+// }
+
+// export default App;
+
+
+
+// this is the method  of use effect and the use state effect //
+
 const App=()=>{
-  const [name,setName]= useState("Sakshi");
-  return(
-    <>
-    <h1>hello Everyone: {name}</h1>
-    <button onClick={()=>{setName("Minakshi")}}>Click here</button>
+ const[cnt,setCnt]=useState(0)
+ const[multi,setMulti]=useState(2)
 
-    </>
-  )
+
+useEffect(()=>{
+  setMulti(cnt*2)
+},[cnt])
+
+
+
+ return(
+  <>
+ <h1> this is the effentn method {cnt}</h1>
+ <h1> this is the multiplication {multi}</h1>
+ <button onClick={()=>{setCnt(cnt+1)}}>CLICK HERE</button>
+  </>
+ )
 }
+
 export default App;
-
-// ===========================================================================
-
-import { useState } from "react";
-const App=()=>{
-  const [color,setColor]= useState("Red");
-
-  const myColor=()=>{
-    setColor("Green")
-  }
-  return(
-    <>
-    <h1>My fav Color: {color}</h1>
-    <button onClick={myColor}>Click here</button>
-    </>
-  )
-}
-export default App;
-
-import { useState } from "react";
-const App=()=>{
-  const [color,setColor]=useState("red");
-  return(
-    <>
-    <h1 style={{color:color}}>My fav color: {color}</h1>
-    <button onClick={()=>{setColor("green")}}>Green color</button>
-    <button onClick={()=>{setColor("red")}}>Red color</button>
-
-    <button onClick={()=>{setColor("blue")}}>Blue color</button>
-    <button onClick={()=>{setColor("pink")}}>Pink color</button>
-    <button onClick={()=>{setColor("yellow")}}>Yellow color</button>
-
-    </>
-  )
-}
-export default App;
-
-import { useState } from "react";
-const App=()=>{
-  const [cnt, setCnt]=useState(0);
-  const myInc=()=>{
-    setCnt(cnt+1)
-  }
-  const myDec=()=>{
-    setCnt(cnt-1)
-  }
-  const myReset=()=>{
-    setCnt(0)
-  }
-  return(
-    <>
-      <center>
-    <h1>Welcome to Counter App</h1>
-    <h2> My Count: {cnt}</h2>
-    <button onClick={myInc}>Increment</button>
-    <button onClick={myDec}>Decrement</button>
-    <button onClick={myReset}>Reset</button>
-    </center>
-    </>
-  )
-}
-export default App;
-
-
